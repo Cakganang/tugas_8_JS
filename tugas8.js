600e746f8acf8846634de71ccc753f4bd45b0008
sonny faristrianto <sonnyf4r15@gmail.com>
2024-01-08 20:30:24 +0700

    tambahkan tugas8

---
+++ tugas8.js
@@ -0,0 +1,12 @@
+function tampilkanObject(){
+    var motor = {
+        tipe: '4tak',
+        jenis: 'bebek',
+        warna: 'hitam',
+    }
+    console.log(motor);
+    motor.warna='merah'
+    console.log(motor);
+}
+
+tampilkanObject()
